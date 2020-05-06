ff73077fa51c418643d27d6c3e14c231701c4446
bizeasy17 <jie.han@outlook.com>
2020-05-06 21:57:28 +0800

    stage it

--- analysis/strategy_jiuzhuan.py
+++ analysis/strategy_jiuzhuan.py
@@ -1,20 +1,23 @@
 
 import tushare as ts
+import pandas as pd
 import time
-from datetime import date,datetime,timedelta
+from datetime import date, datetime, timedelta
 from investors.models import StockFollowing, TradeStrategy
 from stockmarket.models import StockNameCodeMap
 
 pro = ts.pro_api()
 
+
 def trade_calendar(exchange, start_date, end_date):
-    #获取20200101～20200401之间所有有交易的日期
+    # 获取20200101～20200401之间所有有交易的日期
     df = pro.trade_cal(exchange=exchange, is_open='1',
-                            start_date=start_date,
-                            end_date=end_date,
-                            fields='cal_date')
+                       start_date=start_date,
+                       end_date=end_date,
+                       fields='cal_date')
     return df
-    #print(df.head())
+    # print(df.head())
+
 
 def recon_strategy_usage():
     '''
@@ -22,17 +25,52 @@ def recon_strategy_usage():
     '''
     pass
 
+
 def mark_jiuzhuan_listed():
     '''
     对于未标注九转的上市股票运行一次九转序列标记，
     每次运行只是增量上市股票标记
     '''
     today = date.today()
-    listed_companies = StockNameCodeMap.objects.filter(is_marked_jiuzhuan=False)
-    if listed_companies is not None and len(listed_companies)>0:
+    listed_companies = StockNameCodeMap.objects.filter(
+        is_marked_jiuzhuan=False)
+    if listed_companies is not None and len(listed_companies) > 0:
         for listed_company in listed_companies:
-            df = hist_since_listed(listed_company.ts_code, listed_company.list_date.strftime('%Y%m%d'))
+            df = hist_since_listed(
+                listed_company.ts_code, listed_company.list_date.strftime('%Y%m%d'))
             mark_jiuzhuan(df)
+
+
+def split_trade_cal(start_date, end_date):
+    '''
+    如果取数的时间跨度大于10年，就需要对时间进行拆分
+    譬如：
+    1. 19910901 - 20050901，返回的是[(19910901,20010831),(20010901,20050901)]
+    2. 19910901 - 20010901，返回的是[(19910901,20010901)]
+    3. 19910901 - 20200501，返回的是[(19910901,20010831),(20010901,20110901),(20110902,20200501)]
+    '''
+    split_date_list = []
+    start_year = start_date.year
+    end_year = end_date.year
+    if end_year - start_year <= 10:
+        split_date_list.append([start_date, end_date])
+    elif end_year - start_year <= 20:
+        mid_date = end_year + timedelta(days=365 * 10)
+        split_date_list.append(
+            [start_date, mid_date],
+            [mid_date + timedelta(days=1), start_date]
+        )
+    elif end_year - start_year <= 30:
+        mid_date = end_year + timedelta(days=365*10)
+        split_date_list.append(
+            [start_date, mid_date],
+            [mid_date + timedelta(days=1), mid_date +
+             timedelta(days=365*10) + timedelta(days=1)],
+            [mid_date + timedelta(days=365*10) + timedelta(days=1),
+             end_date],
+        )
+    return split_date_list
+
 
 def hist_since_listed(stock_symbol, start_date='', end_date='', freq='D'):
     '''
@@ -40,10 +78,14 @@ def hist_since_listed(stock_symbol, start_date='', end_date='', freq='D'):
     再按照时间先后顺序拼接
     '''
     # pro = ts.pro_api()
-    df = ts.pro_bar(ts_code=stock_symbol, adj=None, freq=freq,
-                    start_date=start_date, end_date=end_date)
-    df = df.iloc[::-1]  # 将数据按照时间顺序排列
-    return df
+    split_cal_list = split_trade_cal(start_date, end_date)
+    df_list = []
+    for trade_cal in split_cal_list:
+        df = ts.pro_bar(ts_code=stock_symbol, adj=None, freq=freq,
+                        start_date=trade_cal[0], end_date=trade_cal[1])
+        df = df.iloc[::-1]  # 将数据按照时间顺序排列
+        df_list.append(df)
+    return pd.concat(df_list)
 
 
 def mark_jiuzhuan(df):
@@ -74,6 +116,7 @@ def mark_jiuzhuan(df):
         time.sleep(1)
     else:
         return df
+
 
 def get_daily(self, ts_code='', trade_date='', start_date='', end_date=''):
     for _ in range(3):
