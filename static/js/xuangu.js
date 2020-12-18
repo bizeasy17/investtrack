64291f6d02524dbc276122d25e192384733480a7
bizeasy17 <jie.han@outlook.com>
2020-12-18 16:42:37 +0800

    xuangu strategy切换

--- static/js/xuangu.js
+++ static/js/xuangu.js
@@ -18,6 +18,8 @@ $(function () {
     var investorEndpoint = "/investors/";
     var totalRowCount = 0
     var prevPagination;
+    var bstr;
+    var sstr;
 
     var selStockName;
     var selStockCode;
@@ -31,6 +33,7 @@ $(function () {
         pickDay = $('input:radio[name="pick-day"]:checked').val();
         period = $('input:radio[name="stk-period"]:checked').val();
         expPct = $('input:radio[name="stk-pct_period"]:checked').val();
+        bstr = $('input:radio[name="bstrategy"]');
     }
 
     var followStock = function (symbol, name) {
@@ -81,149 +84,149 @@ $(function () {
                 $(data.value).each(function (idx, pkStocks) {
                     var content = "";
                     var pctInd = ""
-                    if(parseFloat(pkStocks.chg_pct) < 0){
+                    if (parseFloat(pkStocks.chg_pct) < 0) {
                         pctInd = "text-success";
-                    }else{
+                    } else {
                         pctInd = "text-danger";
                     }
-                    content += 
-                        '<div class="row">'+
-                            '<div class="col-lg-2">'+
-                                '<div class="small">'+
-                                    '<a href="#" class="text-dark">' + pkStocks.stockname + '</a><span class="small text-muted" id=""> '+ pkStocks.ts_code +'</span>'+
-                                    // '<div class="card-subtitle small text-muted"><a href="#" class="text-dark">'+pkStocks.ts_code+'</a></div>'+
-                                '</div>'+
-                                '<div class="small">'+
-                                    '<span class="small text-muted" id="xxx">价:'+pkStocks.price+'</span>'+
-                                    '<span class="small '+pctInd+'" id="xxx"> 涨跌: ' + pkStocks.chg_pct +'%</span>'+
-                                '</div>'+
-                                '<div class="small">'+
-                    '<span class="small text-light btn btn-sm btn-info follow" id="' + pkStocks.ts_code + ',' + pkStocks.stockname + '">加自选</span>'+
-                                '</div>'+
-                            '</div>';
+                    content +=
+                        '<div class="row">' +
+                        '<div class="col-lg-2">' +
+                        '<div class="small">' +
+                        '<a href="#" class="text-dark">' + pkStocks.stockname + '</a><span class="small text-muted" id=""> ' + pkStocks.ts_code + '</span>' +
+                        // '<div class="card-subtitle small text-muted"><a href="#" class="text-dark">'+pkStocks.ts_code+'</a></div>'+
+                        '</div>' +
+                        '<div class="small">' +
+                        '<span class="small text-muted" id="xxx">价:' + pkStocks.price + '</span>' +
+                        '<span class="small ' + pctInd + '" id="xxx"> 涨跌: ' + pkStocks.chg_pct + '%</span>' +
+                        '</div>' +
+                        '<div class="small">' +
+                        '<span class="small text-light btn btn-sm btn-info follow" id="' + pkStocks.ts_code + ',' + pkStocks.stockname + '">加自选</span>' +
+                        '</div>' +
+                        '</div>';
 
-                    content += 
-                            '<div class="col-lg-3">'+
-                                '<div class="content-wrapper small">'+
-                                    '<div class="row">'+
-                                        '<div class="col-lg-2">(天)</div>'+
-                                        '<div class="col-lg-2">25ile</div>'+
-                                        '<div class="col-lg-2">50ile</div>'+
-                                        '<div class="col-lg-2">75ile</div>'+
-                                        '<div class="col-lg-2">Mean</div>' +
-                                        '<div class="col-lg-2">Best</div>' +
-                                    '</div>';
-                    
-                    for(var i=0;i<pkStocks.qt_uppct.length;i++){  
+                    content +=
+                        '<div class="col-lg-3">' +
+                        '<div class="content-wrapper small">' +
+                        '<div class="row">' +
+                        '<div class="col-lg-2">(天)</div>' +
+                        '<div class="col-lg-2">25ile</div>' +
+                        '<div class="col-lg-2">50ile</div>' +
+                        '<div class="col-lg-2">75ile</div>' +
+                        '<div class="col-lg-2">Mean</div>' +
+                        '<div class="col-lg-2">Best</div>' +
+                        '</div>';
+
+                    for (var i = 0; i < pkStocks.qt_uppct.length; i++) {
                         var obj = pkStocks.qt_uppct[i];
-                        content +=              
-                                    '<div class="row">'+
-                                        '<div class="col-lg-2">'+
-                                            obj.period + 
-                                        '</div>'+
-                                        '<div class="col-lg-2">'+
-                                            obj.qt25ile +
-                                        '</div>'+
-                                        '<div class="col-lg-2">'+
-                                            obj.qt50ile +
-                                        '</div>'+
-                                        '<div class="col-lg-2">'+
-                                            obj.qt75ile +
-                                        '</div>'+
-                                        '<div class="col-lg-2">' +
-                                            obj.mean +
-                                        '</div>'+
-                                        '<div class="col-lg-2">' +
-                                            obj.max +
-                                        '</div>'+
-                                    '</div>';
+                        content +=
+                            '<div class="row">' +
+                            '<div class="col-lg-2">' +
+                            obj.period +
+                            '</div>' +
+                            '<div class="col-lg-2">' +
+                            obj.qt25ile +
+                            '</div>' +
+                            '<div class="col-lg-2">' +
+                            obj.qt50ile +
+                            '</div>' +
+                            '<div class="col-lg-2">' +
+                            obj.qt75ile +
+                            '</div>' +
+                            '<div class="col-lg-2">' +
+                            obj.mean +
+                            '</div>' +
+                            '<div class="col-lg-2">' +
+                            obj.max +
+                            '</div>' +
+                            '</div>';
                     }
                     content +=
-                                '</div>'+
-                            '</div>';
-                    
-                    content += 
-                            '<div class="col-lg-3">'+
-                                '<div class="content-wrapper small">'+
-                                    '<div class="row">'+
-                                        '<div class="col-lg-2">(天)</div>'+
-                                        '<div class="col-lg-2">25ile</div>'+
-                                        '<div class="col-lg-2">50ile</div>'+
-                                        '<div class="col-lg-2">75ile</div>'+
-                                        '<div class="col-lg-2">Mean</div>'+
-                                        '<div class="col-lg-2">Worst</div>'+
-                                    '</div>';
-                    
-                    for(var i=0;i<pkStocks.qt_downpct.length;i++){  
+                        '</div>' +
+                        '</div>';
+
+                    content +=
+                        '<div class="col-lg-3">' +
+                        '<div class="content-wrapper small">' +
+                        '<div class="row">' +
+                        '<div class="col-lg-2">(天)</div>' +
+                        '<div class="col-lg-2">25ile</div>' +
+                        '<div class="col-lg-2">50ile</div>' +
+                        '<div class="col-lg-2">75ile</div>' +
+                        '<div class="col-lg-2">Mean</div>' +
+                        '<div class="col-lg-2">Worst</div>' +
+                        '</div>';
+
+                    for (var i = 0; i < pkStocks.qt_downpct.length; i++) {
                         var obj = pkStocks.qt_downpct[i];
-                        content +=              
-                                    '<div class="row">'+
-                                        '<div class="col-lg-2">'+
-                                            obj.period +
-                                        '</div>'+
-                                        '<div class="col-lg-2">'+
-                                            obj.qt25ile +
-                                        '</div>'+
-                                        '<div class="col-lg-2">'+
-                                            obj.qt50ile +
-                                        '</div>'+
-                                        '<div class="col-lg-2">'+
-                                            obj.qt75ile +
-                                        '</div>'+
-                                        '<div class="col-lg-2">' +
-                                            obj.mean +
-                                        '</div>'+
-                                        '<div class="col-lg-2">' +
-                                            obj.min +
-                                        '</div>'+
-                                    '</div>';
-                    }
-                    content +=
-                                '</div>'+
+                        content +=
+                            '<div class="row">' +
+                            '<div class="col-lg-2">' +
+                            obj.period +
+                            '</div>' +
+                            '<div class="col-lg-2">' +
+                            obj.qt25ile +
+                            '</div>' +
+                            '<div class="col-lg-2">' +
+                            obj.qt50ile +
+                            '</div>' +
+                            '<div class="col-lg-2">' +
+                            obj.qt75ile +
+                            '</div>' +
+                            '<div class="col-lg-2">' +
+                            obj.mean +
+                            '</div>' +
+                            '<div class="col-lg-2">' +
+                            obj.min +
+                            '</div>' +
                             '</div>';
-                    
-                    content += 
-                            '<div class="col-lg-4">'+
-                                '<div class="content-wrapper small">'+
-                                    '<div class="row">'+
-                                        '<div class="col-lg-2">涨幅</div>'+
-                                        '<div class="col-lg-2">25ile</div>'+
-                                        '<div class="col-lg-2">50ile</div>'+
-                                        '<div class="col-lg-2">75ile</div>'+
-                                        '<div class="col-lg-2">Mean</div>'+
-                                        '<div class="col-lg-2">Best</div>'+
-                                    '</div>';
-                    
-                    for(var i=0;i<pkStocks.qt_targetpct.length;i++){  
-                        var obj = pkStocks.qt_targetpct[i];
-                        content +=              
-                                    '<div class="row">'+
-                                        '<div class="col-lg-2">'+
-                                            obj.pct +
-                                        '</div>'+
-                                        '<div class="col-lg-2">'+
-                                            obj.qt25ile +
-                                        '</div>'+
-                                        '<div class="col-lg-2">'+
-                                            obj.qt50ile +
-                                        '</div>'+
-                                        '<div class="col-lg-2">'+
-                                            obj.qt75ile +
-                                        '</div>'+
-                                        '<div class="col-lg-2">' +
-                                            obj.mean +
-                                        '</div>'+
-                                        '<div class="col-lg-2">' +
-                                            obj.min +
-                                        '</div>'+
-                                    '</div>';
                     }
                     content +=
-                                '</div>'+
-                            '</div>'+
-                        '</div>'+
+                        '</div>' +
+                        '</div>';
+
+                    content +=
+                        '<div class="col-lg-4">' +
+                        '<div class="content-wrapper small">' +
+                        '<div class="row">' +
+                        '<div class="col-lg-2">涨幅</div>' +
+                        '<div class="col-lg-2">25ile</div>' +
+                        '<div class="col-lg-2">50ile</div>' +
+                        '<div class="col-lg-2">75ile</div>' +
+                        '<div class="col-lg-2">Mean</div>' +
+                        '<div class="col-lg-2">Best</div>' +
+                        '</div>';
+
+                    for (var i = 0; i < pkStocks.qt_targetpct.length; i++) {
+                        var obj = pkStocks.qt_targetpct[i];
+                        content +=
+                            '<div class="row">' +
+                            '<div class="col-lg-2">' +
+                            obj.pct +
+                            '</div>' +
+                            '<div class="col-lg-2">' +
+                            obj.qt25ile +
+                            '</div>' +
+                            '<div class="col-lg-2">' +
+                            obj.qt50ile +
+                            '</div>' +
+                            '<div class="col-lg-2">' +
+                            obj.qt75ile +
+                            '</div>' +
+                            '<div class="col-lg-2">' +
+                            obj.mean +
+                            '</div>' +
+                            '<div class="col-lg-2">' +
+                            obj.min +
+                            '</div>' +
+                            '</div>';
+                    }
+                    content +=
+                        '</div>' +
+                        '</div>' +
+                        '</div>' +
                         '<hr/>';
-                    container.append(content);  
+                    container.append(content);
                 });
 
                 $(".follow").click(function () {
@@ -232,10 +235,10 @@ $(function () {
 
                 $(container).append(
                     '<div class="row">' +
-                        '<div class="col-lg-12">' +
-                        '<div class="card-title small"><a href="#" class="text-dark">选股结果共: '+data.row_count+' 条</a><span class="small text-muted" id="xxx"></span></div>' +
-                        '<div class="card-subtitle small text-muted">当前: '+(currentIdx+1)+' - '+ (currentIdx + rowCount)+' 条<a href="#" class="text-dark"></a></div>' +
-                        '</div>' +
+                    '<div class="col-lg-12">' +
+                    '<div class="card-title small"><a href="#" class="text-dark">选股结果共: ' + data.row_count + ' 条</a><span class="small text-muted" id="xxx"></span></div>' +
+                    '<div class="card-subtitle small text-muted">当前: ' + (currentIdx + 1) + ' - ' + (currentIdx + rowCount) + ' 条<a href="#" class="text-dark"></a></div>' +
+                    '</div>' +
                     '</div>'
                 );
                 totalRowCount = data.row_count;
@@ -295,6 +298,12 @@ $(function () {
     // 根据选择的期望收益，显示达到期望收益的天数
     $('input:radio[name="bstrategy"]').change(function () {
         // 页面默认加载上证指数日K（D)
+        bstr = $(this)
+        if (sstr != undefined) {
+            $(sstr).removeAttr("checked")
+            $(sstr).parent("label").removeClass("active");
+        } 
+
         pickStrategy = this.value;
         bandPickingTable(pickStrategy, pickYear, pickMon, pickDay, period, expPct, startIdx, rowCount);
     });
@@ -302,6 +311,11 @@ $(function () {
     // 根据选择的期望收益，显示达到期望收益的天数
     $('input:radio[name="sstrategy"]').change(function () {
         // 页面默认加载上证指数日K（D)
+        sstr = $(this);
+        if (bstr != undefined) {
+            $(bstr).removeAttr("checked")
+            $(bstr).parent("label").removeClass("active");
+        } 
         pickStrategy = this.value;
         bandPickingTable(pickStrategy, pickYear, pickMon, pickDay, period, expPct, startIdx, rowCount);
     });
@@ -338,14 +352,14 @@ $(function () {
             // next
             if (currentIdx + rowCount + 1 > totalRowCount) {
                 return;
-            } 
+            }
             // if (totalRowCount - currentIdx > 1 && totalRowCount - currentIdx - rowCount < rowCount) {
             //     rowCount = totalRowCount - currentIdx - rowCount;
             // } 
             currentIdx += rowCount;
             console.log(currentIdx);
         }
-        
+
         prevPagination = $(this);
         bandPickingTable(pickStrategy, pickYear, pickMon, pickDay, period, expPct, currentIdx, currentIdx + rowCount);
     });
